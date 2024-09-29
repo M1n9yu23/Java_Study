aeb9ad7d88c97c8ae9a2f06ee50bcd2ad13fb19d
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-09-29 16:14:49 +0900

    2024-09-29 ch15 section02 Add

--- src/ch15/sec02/exam03/LinkedListExample.java
+++ src/ch15/sec02/exam03/LinkedListExample.java
@@ -20,8 +20,6 @@ public class LinkedListExample {
         System.out.printf("ArrayList 걸린 시간: %8d ns \n", (endTime-startTime));
         System.out.println(list2.get(0));
 
-
-
         startTime = System.nanoTime();
         for(int i = 0; i<1000; i++){
             list1.add(0, String.valueOf(i)); // 인덱스 0에 계속 넣기 때문에 처음에 넣은 0은 뒤로 밀려남 인덱스 999에 0이 들어있음.
@@ -30,7 +28,6 @@ public class LinkedListExample {
         System.out.printf("LinkedList 걸린 시간: %8d ns \n", (endTime-startTime));
 
         System.out.println(list1.get(999));
-
 
     }
 }
