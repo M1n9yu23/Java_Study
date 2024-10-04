fd10f78ab3b38e4b8d379d22dbc5c9901d1a1500
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-10-04 15:24:10 +0900

    2024-10-03 ch17 section06 Add

---
+++ src/ch17/sec06/exam02/MapExample.java
@@ -0,0 +1,24 @@
+package ch17.sec06.exam02;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class MapExample {
+    public static void main(String[] args) {
+        int[] ints = {1,2,3,4,5};
+
+        IntStream intStream = Arrays.stream(ints);
+        intStream.asDoubleStream().forEach(i -> System.out.println(i));
+        // 정수 스트림을 실수 스트림으로 변환
+
+        // intStream.forEach(i -> System.out.println(i));
+        // 스트림을 재사용 못함.
+
+        System.out.println();
+
+        // 사용한 스트림을 다시 사용하기 위해 스트림을 다시 얻음.
+        intStream = Arrays.stream(ints);
+        intStream.boxed().forEach(obj -> System.out.println(obj.intValue()));
+        // 기본타입 스트림을 참조타입 스트림으로 변환 즉 기본 스트림 -> 래퍼 스트림
+    }
+}
