4b0c67d87f0a3b38b2efbdb5f1e73b25631413e2
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-10-08 22:54:31 +0900

    2024-10-08 ch18 section11 Add

---
+++ src/ch18/sec11/FilesExample.java
@@ -0,0 +1,28 @@
+package ch18.sec11;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FilesExample {
+    public static void main(String[] args) {
+        try{
+            String data = "" + "id: winter\n" + "email: winter@mycompany.com\n" +
+                    "tel: 010-123-1234";
+            Path path = Paths.get("C:\\study\\java\\Java_Study\\src\\ch18\\sec11\\TestDir\\user.txt");
+
+            Files.writeString(path, data, Charset.forName("UTF-8"));
+
+            System.out.println("파일 유형: " + Files.probeContentType(path)); // 파일 확장명에 따른 파일 유형을 리턴
+            System.out.println("파일 크기: " + Files.size(path) + " bytes");
+
+            String content = Files.readString(path, Charset.forName("UTF-8"));
+            System.out.println(content);
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
